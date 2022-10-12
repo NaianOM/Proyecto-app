955687ed192f36b65ca5833395a62b57a7afbcbf
NaianOM <otanez.montes.naian.madai@gmail.com>
2022-10-12 02:11:41 -0500

    first

---
+++ MySplash/app/src/main/java/com/example/mysplash/json/MyInfo.java
@@ -0,0 +1,55 @@
+package com.example.mysplash.json;
+
+import java.io.Serializable;
+
+public class MyInfo implements Serializable {
+    private String nombre;
+    private String contrasena;
+    private  String email;
+    private int telefono;
+    private String descripcion;
+
+    public MyInfo(){}
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
+
+
+
