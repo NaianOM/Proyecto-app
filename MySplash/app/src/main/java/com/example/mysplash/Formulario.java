e95567521be2d077de2a559a0130f4bc9b0d0f12
NaianOM <112534323+NaianOM@users.noreply.github.com>
2022-10-16 21:16:26 -0500

    Add files via upload

--- MySplash/app/src/main/java/com/example/mysplash/Formulario.java
+++ MySplash/app/src/main/java/com/example/mysplash/Formulario.java
@@ -5,16 +5,184 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
-public class Formulario extends AppCompatActivity {
+import java.io.Serializable;
+
+public class Formulario extends AppCompatActivity implements Serializable {
+
+    public String nombre;
+    private String contrasena;
+    private  String correo;
+    private int telefono;
+    private String fecha;
+    private String descripcion;
+    private float altura;
+    private Boolean terminos;
+    private Boolean politicas;
+    private Boolean radio_si, radio_no;
+    private Boolean enfermedad;
+
+    public Boolean getEnfermedad() {
+        return enfermedad;
+    }
+    public void setEnfermedad(Boolean enfermedad) {
+        this.enfermedad = enfermedad;
+    }
+    public Boolean getRadio_no() {
+        return radio_no;
+    }
+    public void setRadio_no(Boolean radio_no) {
+        this.radio_no = radio_no;
+    }
+    public Boolean getRadio_si() {
+        return radio_si;
+    }
+    public void setRadio_si(Boolean radio_si) {
+        this.radio_si = radio_si;
+    }
+    public Boolean getPoliticas() {
+        return politicas;
+    }
+    public void setPoliticas(Boolean politicas) {
+        this.politicas = politicas;
+    }
+    public Boolean getTerminos() {
+        return terminos;
+    }
+    public void setTerminos(Boolean terminos) {
+        this.terminos = terminos;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getContrasena() {
+        return contrasena;
+    }
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+    public String getCorreo() {
+        return correo;
+    }
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public String getFecha() {
+        return fecha;
+    }
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public float getAltura() {
+        return altura;
+    }
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        EditText nombre = findViewById(R.id.usuario_registro);
+        EditText contrasena = findViewById(R.id.contrasena_usuario);
+        EditText correo = findViewById(R.id.correo_usuario);
+        EditText telefono = findViewById(R.id.telefono);
+        EditText descrip = findViewById(R.id.descripcion);
+        CheckBox terminos = (CheckBox) findViewById(R.id.terminos);
+        CheckBox politicas = (CheckBox) findViewById(R.id.politicas);
+        RadioButton radio_si = (RadioButton) findViewById(R.id.radio_si);
+        RadioButton radio_no = (RadioButton) findViewById(R.id.radio_no);
+        Switch enfermedad = (Switch) findViewById(R.id.enfermedad);
+
+
+    }
+    public void Registrar(View view){
+        Intent Registrar = new Intent(this, Login.class);
+
+        Registrar.putExtra("radio_si", getRadio_si());
+        Registrar.putExtra("radio_no", getRadio_no());
+        Registrar.putExtra("enfermedad", getEnfermedad());
+        if(nombre.length() != 0) {
+            Registrar.putExtra("nombre", getNombre().toString());
+            startActivity(Registrar);
+        } else{
+            Toast.makeText(this,"Debes ingresar un nombre",Toast.LENGTH_LONG).show();
+        }
+        if(contrasena.length() != 0) {
+            Registrar.putExtra("contrasena", getContrasena().toString());
+            startActivity(Registrar);
+        } else{
+            Toast.makeText(this,"Debes ingresar una contraseña",Toast.LENGTH_LONG).show();
+        }
+        if(correo.length() != 0) {
+            Registrar.putExtra("correo", getCorreo().toString());
+            startActivity(Registrar);
+        } else{
+            Toast.makeText(this,"Debes ingresar una dirección de correo",Toast.LENGTH_LONG).show();
+        }
+        if(telefono != 0) {
+            Registrar.putExtra("telefono", getTelefono());
+            startActivity(Registrar);
+        } else{
+            Toast.makeText(this,"Debes ingresar un número de teléfono",Toast.LENGTH_LONG).show();
+        }
+        if(fecha.length() != 0) {
+            Registrar.putExtra("fecha", getFecha().toString());
+            startActivity(Registrar);
+        } else{
+            Toast.makeText(this,"Debes ingresar un número de teléfono",Toast.LENGTH_LONG).show();
+        }
+
+        if(descripcion.length() != 0) {
+            Registrar.putExtra("descripcion", getDescripcion().toString());
+            startActivity(Registrar);
+        } else{
+            Toast.makeText(this,"Debes ingresar una pequeña descripción",Toast.LENGTH_LONG).show();
+        }
+
+        if(altura != 0) {
+            Registrar.putExtra("altura", getAltura());
+            startActivity(Registrar);
+        } else{
+            Toast.makeText(this,"Debes ingresar tu altura",Toast.LENGTH_LONG).show();
+        }
+
+        if(terminos = true) {
+            Registrar.putExtra("terminos", getTerminos());
+            startActivity(Registrar);
+        } else{
+            Toast.makeText(this,"Debes haber leído los términos y condiciiones",Toast.LENGTH_LONG).show();
+        }
+        if(politicas = true) {
+            Registrar.putExtra("politicas", getPoliticas());
+            startActivity(Registrar);
+        } else{
+            Toast.makeText(this,"Debes haber leído las políticas",Toast.LENGTH_LONG).show();
+        }
     }
     public void Regresar(View view){
-        Intent Regresar = new Intent(this, MainActivity.class);
+        Intent Regresar = new Intent(this, Login.class);
         startActivity(Regresar);
     }
 }
--- MySplash/app/src/main/java/com/example/mysplash/MainActivity.java
+++ MySplash/app/src/main/java/com/example/mysplash/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.mysplash.json.MyInfo;
@@ -36,90 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, 5000);
-        setContentView(R.layout.activity_main);
-        object2Json( );
-        json2Object( "{\"calificacion\":9.5,\"edad\":17,\"nombre\":\"Gerdoc 1\",\"sexo\":true}" );
-        list2Json( );
-        json2List( "[{\"calificacion\":7.41,\"edad\":16,\"nombre\":\"1 Gerdoc 1\",\"sexo\":true},\n" +
-                "{\"calificacion\":4.98,\"edad\":17,\"nombre\":\"2 Gerdoc 2\",\"sexo\":true}]" );
+
     }
 
-    private void list2Json() {
-    }
-
-    private void object2Json() {
-        MyInfo myInfo = null;
-        Gson gson = null;
-        String json = null;
-        String mensaje = null;
-
-        myInfo = new MyInfo();
-        myInfo.setNombre();
-        myInfo.setContrasena();
-        myInfo.setDescripcion();
-        myInfo.setEmail();
-        myInfo.setTelefono();
-
-        Log.d(TAG, "TEST");
-        gson = new Gson();
-        json = gson.toJson(myInfo);
-        if (json != null) {
-            Log.d(TAG, json);
-            mensaje = "object2Json OK";
-        } else {
-            mensaje = "Error object2Json";
-        }
-        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG).show();
-    }
-    public void json2Object( String json )
-    {
-        MyInfo myInfo2 = null;
-        Gson gson = null;
-        String mensaje = null;
-
-        gson = new Gson();
-
-
-        myInfo2 = gson.fromJson(json, MyInfo.class);
-        if (myInfo2 != null) {
-            mensaje = String.format("(%s)(%d)(%f)(%s)", myInfo2.getNombre(), myInfo2.getContrasena(), myInfo2.getEmail(), myInfo2.getTelefono()
-                    , myInfo2.getDescripcion());
-        } else {
-            mensaje = "Error2";
-        }
-        Log.d(TAG, mensaje);
-        Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG).show();
-    }
-    public void json2List( String json )
-    {
-        List<MyInfo> list = null;
-        Gson gson = null;
-        String mensaje = null;
-        if (json == null || json.length() == 0)
-        {
-            Toast.makeText(getApplicationContext(), "Error json null or empty", Toast.LENGTH_LONG).show();
-            return;
-        }
-        gson = new Gson();
-        Type listType = new TypeToken<ArrayList<MyInfo>>(){}.getType();
-        list = gson.fromJson(json, listType);
-        if (list == null || list.size() == 0 )
-        {
-            Toast.makeText(getApplicationContext(), "Error list is null or empty", Toast.LENGTH_LONG).show();
-            return;
-        }
-        for( MyInfo myInfo : list )
-        {
-            if (myInfo != null)
-            {
-                mensaje = String.format("(%s)(%d)(%f)(%s)", myInfo.getNombre(), myInfo.getContrasena(), myInfo.getEmail()
-                        , myInfo.getTelefono(), myInfo.getDescripcion());
-            }
-            else
-            {
-                mensaje = "Error2";
-            }
-            Log.d(TAG, mensaje);
-        }
-    }
 }
